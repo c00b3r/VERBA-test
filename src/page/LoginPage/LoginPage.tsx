851b92ba275d467ffce04cbb838bbccc827d5c3e
c00b3r <sanuyOKmaklaren0>
2024-11-05 00:51:34 +0500

    feat: add styles for Login page form

--- src/page/LoginPage/LoginPage.tsx
+++ src/page/LoginPage/LoginPage.tsx
@@ -1,8 +1,57 @@
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 export default function LoginPage() {
   const authContext = useContext(AuthContext);
   console.log(authContext);
-  return <div>LoginPage</div>;
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      border={"1px solid black"}
+      p={2}
+      gap={2}
+      borderRadius={"8px"}
+    >
+      <Typography variant="h5">Войти</Typography>
+      <TextField
+        label="Логин"
+        sx={{
+          label: { color: "white" },
+          input: { color: "white" },
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused fieldset": { borderColor: "black" },
+          },
+          "& .MuiInputLabel-root.Mui-focused": { color: "white" },
+        }}
+      />
+      <TextField
+        label="Пароль"
+        type="password"
+        sx={{
+          label: { color: "white" },
+          input: { color: "white" },
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused fieldset": { borderColor: "black" },
+          },
+          "& .MuiInputLabel-root.Mui-focused": { color: "white" },
+        }}
+        color="info"
+      />
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "black",
+          color: "white",
+          "&:hover": {
+            backgroundColor: "#0a0a0a",
+          },
+        }}
+      >
+        Войти
+      </Button>
+    </Box>
+  );
 }
